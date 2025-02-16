24ffe16db85d5e7c3a8de48433820a4846d8b507
djorgeext <djorge.ext@fi.uba.ar>
2025-02-16 14:39:16 -0300

    last

--- romanNumeralConverter/script.js
+++ romanNumeralConverter/script.js
@@ -8,29 +8,60 @@ const output = document.getElementById('output');
 
 // function to convert number to roman numeral
 const convertToRoman = (num, result='') => {
-    const reference = {
-        1000: 'M',
-        900: 'CM',
-        500: 'D',
-        400: 'CD',
-        100: 'C',
-        90: 'XC',
-        50: 'L',
-        40: 'XL',
-        10: 'X',
-        9: 'IX',
-        5: 'V',
-        4: 'IV',
-        1: 'I'
-    }
+    const reference = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ]
 
     if (num === 0) {
         return result;
     } else {
-        for (ref in reference) {
-            if (num >= ref) {
-                return convertToRoman(num - ref, result + reference[ref]);
+        for (const [value, numeral] of reference) {
+            if (num >= value) {
+                return convertToRoman(num - value, result + numeral);
             }
         }
     }
 };
+
+const checkUserInput = () => {
+    const inputInt = parseInt(numberInput.value);
+  
+    if (!numberInput.value || isNaN(inputInt)) {
+        output.classList.toggle('hidden');
+        output.textContent = "Please enter a valid number";
+        return;
+    } else if (inputInt < 1) {
+        output.classList.toggle('hidden');
+        output.textContent = "Please enter a number greater than or equal to 1";
+        return;
+    } else if (inputInt > 3999) {
+        output.classList.toggle('hidden');
+        output.textContent = "Please enter a number less than or equal to 3999";
+        return;
+    } else {
+        output.classList.toggle('hidden');
+        output.textContent = convertToRoman(inputInt);
+    }
+  };
+
+// event listener
+convertBtn.addEventListener('click', checkUserInput);
+numberInput.addEventListener('keydown', (e) => {
+    
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        checkUserInput();
+    }
+});
